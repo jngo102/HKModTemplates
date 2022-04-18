f5e4a7ef2e8aff9cb20a1752cd8be0b49f239ef0
jngo102 <jngo102@aim.com>
2022-04-17 20:33:24 -0400

    Actions pipeline

--- ReadMe.md
+++ ReadMe.md
@@ -19,7 +19,6 @@ There are several parameters you may pass into the `dotnet` command:
 - --modAuthor: The mod's author, i.e. your name. Default is blank.
 - --modDescription: A description of the mod. Default is "A Hollow Knight mod."
 - --hkRefsPath: The location of Hollow Knight's Managed/ folder on your PC. This is the folder that contains the `Assembly-CSharp.dll` file. Default is `$(MSBuildProgramFiles32)/Steam/steamapps/common/Hollow Knight/hollow_knight_Data/Managed/`.
-- --exportPath: Where you want to export your packaged mod to on your PC. Default is `$(MSBuildProgramFiles32)/Steam/steamapps/common/Hollow Knight/hollow_knight_Data/Managed/Mods/Export/`.
 
 e.g: `dotnet new hkmod --modName "MyMod" --modAuthor "My Name" --modDescription "Description of my mod." --hkRefsPath "C:\Program Files (x86)\Steam\steamapps\common\Hollow Knight\hollow_knight_Data\Managed\"`
 
--- Templates/Default/ModName/ModName.cs
+++ Templates/Default/ModName/ModName.cs
@@ -11,18 +11,18 @@ namespace {modName}
     {
         internal static {modName} Instance;
 
-        //public override List<ValueTuple<string, string>> GetPreloadNames()
-        //{
-        //    return new List<ValueTuple<string, string>>
-        //    {
-        //        new ValueTuple<string, string>("White_Palace_18", "White Palace Fly")
-        //    };
-        //}
+        // public override List<ValueTuple<string, string>> GetPreloadNames()
+        // {
+        //     return new List<ValueTuple<string, string>>
+        //     {
+        //         new ValueTuple<string, string>("White_Palace_18", "White Palace Fly")
+        //     };
+        // }
 
-        //public {modName}() : base("{modName}")
-        //{
-        //    Instance = this;
-        //}
+        // public {modName}() : base("{modName}")
+        // {
+        //     Instance = this;
+        // }
 
         public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
         {
